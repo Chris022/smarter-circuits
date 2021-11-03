4304c6b2bb276d40b1da0466ae868df503bc3020
Makei04 <mayrhofer803@gmx.at>
2021-11-03 15:58:42 +0100

    :recycle: refactored and created python files

---
+++ src/featureRecognition.py
@@ -0,0 +1,65 @@
+import numpy
+import math
+import filter as ft
+
+def rotateFilters(filters):
+    rotatedFilters = []
+    for filter in filters:
+        for k in range(0,4):
+            rotatedFilters.append(numpy.rot90(filter, k))
+    return rotatedFilters
+
+def getFeatures(image):
+
+    # create filters
+    filters = []
+    filters.append([[0,0,0],[0,255,255],[0,0,0]])
+    filters.append([[255,0,0],[0,255,0],[0,0,0]])
+    endPointFilters = rotateFilters(filters)
+
+    filters = []
+    filters.append([[0,0,0],[255,255,255],[0,255,0]])
+    filters.append([[0,0,0],[255,0,255],[0,255,0]])
+    filters.append([[255,0,0],[0,255,255],[0,255,0]])
+    filters.append([[0,0,255],[255,255,255],[0,255,0]])
+    filters.append([[255,0,0],[255,255,255],[0,255,0]])
+    filters.append([[0,255,0],[0,255,255],[255,255,255]])
+    filters.append([[255,0,255],[0,255,255],[0,255,0]])
+    filters.append([[255,0,255],[255,255,0],[0,255,0]])
+    intersectionFilters = rotateFilters(filters)
+
+    # filter white regions
+    whiteFeatures = ft.whiteFilter(image)
+    points = []
+    for y in range(1, len(whiteFeatures)-1):
+        for x in range(1, len(whiteFeatures[y])-1):
+            if whiteFeatures[y][x] == 0:
+                points.append((x,y))
+
+    # apply endpoint filters
+    endPoints = []
+    for filter in endPointFilters:
+        endPoints = endPoints + ft.applyFilter(filter, image, points)
+
+    # apply intersection filters
+    intersections = []
+    for filter in intersectionFilters:
+        intersections = intersections + ft.applyFilter(filter, image, points)
+
+    # remove unnecessary intersections
+    filteredIntersections = []
+
+    filteredIntersections.append(intersections[0])
+    intersections.remove(intersections[0])
+
+    for inter in intersections:
+        connected = False
+        for filtered in filteredIntersections:
+            dist = math.sqrt((inter[0] - filtered[0])**2 + (inter[1] - filtered[1])**2)
+            if dist < 2:
+                connected = True
+        if not connected:
+            filteredIntersections.append(inter)
+
+    return endPoints, filteredIntersections
+
