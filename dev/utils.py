c18c59930b7b6cca84db9683d5085beb221282ce
unknown <christoph02.cw@gmail.com>
2021-11-24 14:57:38 +0100

    :construction: use new utils

--- dev/utils.py
+++ dev/utils.py
@@ -1,34 +1,62 @@
-import numpy
-from PIL import Image
+import numpy as np
+import cv2
 
 #create an independent copy of a numpy array
 def createIndependetCopy(img):
-    newImg = numpy.empty_like(img)
+    newImg = np.empty_like(img)
     newImg[:] = img
     return newImg
 
-#turns an image with first x than y cooridates into an image with first y than x and the other way around
-def flipImage(image):
-    image = createIndependetCopy(image)
-    return numpy.transpose(image)
+def zip2d(arg0, arg1, arg2):
+    arr = np.zeros((len(arg0),len(arg0[0]),3))
+    for x in range(0, len(arr)):
+        for y in range(0, len(arr[x])):
+            arr[x][y] = (arg0[x][y], arg1[x][y], arg2[x][y])
+    return arr
 
-# Function that loads an black and white image and changes the color values so that 0 is white and 1 is black
-# Output Image is a array with first index X and second index Y coorodiante
-def load1BitBWImage(path,name):
-    image = Image.open('{path}/{name}'.format(path=path,name=name)).convert('1')
-    img = 1-numpy.array(image)  
-    return img
+# load image
+def loadImage(path, name, transpose=True, resize=(650, 450), invert=True, color=False, binary=False):
+    image = cv2.imread('{path}/{name}'.format(path=path,name=name))
+    if not color:
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+        if binary:
+            for y in range(0, len(image)):
+                for x in range(0, len(image[y])):
+                    if image[y][x] < 125:
+                        image[y][x] = 0
+                    else:
+                        image[y][x] = 255
 
-# Function that loads an rgb image and changes the color values
-# Output Image is a array with first index X and second index Y coorodiante
-def loadRGBImage(path,name):
-    image = Image.open('{path}/{name}'.format(path=path,name=name))
-    testData = numpy.asarray(image)
-    return createIndependetCopy(testData)
+    image = cv2.resize(image, resize, interpolation = cv2.INTER_AREA)
+    if invert:
+        image = 255 - image
+    if transpose:
+        image = np.transpose(image)
+    return np.asarray(image)
+
+def thinnImage(image):
+    image = cv2.ximgproc.thinning(image)
+    return image
+
+def saveImage(name, image, invert = True, color=False):
+    image = np.transpose(image)
+    if not color:
+        image = cv2.cvtColor(image, cv2.COLOR_GRAY2RGB)
+    if invert:
+        cv2.imwrite('./img/' + name, 255 - image)
+    else:
+        cv2.imwrite('./img/' + name, image)
 
 # Runs the function on every Pixel in the image
 def foreachPixel(image,function):
     for y in range(0,len(image)):
         for x in range(0,len(image[y])):
-            if image[y][x]:
-                function(image[y][x])
+            function(image[y][x])
+
+# apply a function to every pixel
+def mapPixel(image,function):
+    newImage = createIndependetCopy(image)
+    for y in range(0,len(image)):
+        for x in range(0,len(image[y])):
+            newImage[y][x] = function(image[y][x])
+    return newImage
