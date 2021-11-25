c38acf3183ac396bb1aefe28bbb219cb78a18fdb
Makai04 <43985854+Makei04@users.noreply.github.com>
2021-11-25 09:19:37 +0100

    :recycle: changed x y coordinates stoped using inverted images

--- src/imagePreprocessing.py
+++ src/imagePreprocessing.py
@@ -1,22 +1,26 @@
 import numpy as np
 import cv2
 import utils
+import math
 
-def getRegion(pos, image):
-    return np.transpose(image[pos[0]-1:pos[0]+2, pos[1]-1:pos[1]+2])
+def getRegion(size, pos, image):
+    xOff = math.floor(size[0]/2)
+    yOff = math.floor(size[1]/2)
+    return image[pos[1]-yOff:pos[1]+yOff+1, pos[0]-xOff:pos[0]+xOff+1]
 
 def equal(filter, img):
-    for x in range(0, len(filter)):
-        for y in range(0, len(filter[0])):
-            if filter[x][y] != -1 and filter[x][y] != img[x][y]:
+    for y in range(0, len(filter)):
+        for x in range(0, len(filter[y])):
+            if filter[y][x] != -1 and filter[y][x] != img[y][x]:
                 return False
     return True
 
 def applyFilterOnPoints(filter, image, points):
     features = []
+    size = [len(filter), len(filter[0])]
     for (x, y) in points:
         try:
-            if equal(filter, getRegion((x, y), image)):
+            if equal(filter, getRegion(size, (x, y), image)):
                 features.append((x,y))
         except:
             pass
@@ -37,36 +41,34 @@ def addFeature(feature, replacement, rotate=True, flip=False):
             features.append([feature[i], replacement[i]])
     else:
         features.append([feature, replacement])
-
     if flip:
         flipped = []
         for i in range(0, len(features)):
-            flipped += [np.flip(features[i][0],1), np.flip(features[i][1],1)]
-            pass 
+            flipped.append([np.flip(features[i][0],1), np.flip(features[i][1],1)])
         features += flipped
     return features
 
 def getFeatures():
     features = []
-
-    feature = [[0,255,0],[255,255,255],[0,255,0]]
-    replacement = [[255,0,0],[0,0,0],[255,0,255]]
+    
+    feature = [[255,0,255],[0,0,0],[255,0,255]]
+    replacement = [[0,255,255],[255,255,255],[0,255,0]]
     features += addFeature(feature, replacement, rotate=False)
 
-    feature = [[0,255,-1],[-1,255,255],[255,255,-1]]
-    replacement = [[0,255,255],[-1,255,0],[255,255,-1]]
+    feature = [[255,0,-1],[-1,0,0],[0,0,-1]]
+    replacement = [[255,0,0],[-1,0,255],[0,0,-1]]
     features += addFeature(feature, replacement, flip=True)
 
-    feature = [[-1,0,255],[255,255,0],[0,255,-1]]
-    replacement = [[-1,0,255],[255,0,255],[0,255,-1]]
+    feature = [[-1,255,0],[0,0,255],[255,0,-1]]
+    replacement = [[-1,255,0],[0,255,0],[255,0,-1]]
     features += addFeature(feature, replacement)
-
-    feature = [[-1,0,-1],[255,255,255],[0,255,0]]
-    replacement = [[-1,0,-1],[255,0,255],[0,255,0]]
+    
+    feature = [[-1,255,-1],[0,0,0],[255,0,255]]
+    replacement = [[-1,255,-1],[0,255,0],[255,0,255]]
     features += addFeature(feature, replacement)
-
-    feature = [[-1,0,0],[255,255,0],[0,255,-1]]
-    replacement = [[-1,0,0],[255,0,0],[0,255,-1]]
+    
+    feature = [[-1,255,255],[0,0,255],[255,0,-1]]
+    replacement = [[-1,255,255],[0,255,255],[255,0,-1]]
     features += addFeature(feature, replacement)
 
     return features
@@ -76,16 +78,16 @@ def preprocessImage(image):
     (thresh, binary) = cv2.threshold(image, 100, 255, cv2.THRESH_BINARY)
 
     points = []
-    for x in range(0, len(binary)):
-        for y in range(0, len(binary[x])):
-            if binary[x][y] == 255:
+    for y in range(0, len(binary)):
+        for x in range(0, len(binary[y])):
+            if binary[y][x] == 0:
                 points.append((x,y))
 
-    filter = np.full((13, 13), 255)
+    filter = np.full((13, 13), 0)
     features = applyFilterOnPoints(filter, binary, points)
 
     for (x,y) in features:
-        binary[x][y] = 0
+        binary[y][x] = 255
 
     thinn = utils.thinnImage(binary)
 
@@ -97,6 +99,6 @@ def preprocessImage(image):
             for y in range(0, len(feature[1])):
                 for x in range(0, len(feature[1][y])):
                     if feature[1][y][x] != -1:
-                        thinn[xOff + x - 1][yOff + y - 1] = feature[1][y][x]
+                        thinn[yOff + y - 1][xOff + x - 1] = feature[1][y][x]
 
     return thinn
