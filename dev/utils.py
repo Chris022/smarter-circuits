b885c1430b087ab76d8131b418fa4a71f92161d9
unknown <christoph02.cw@gmail.com>
2021-11-25 12:27:04 +0100

    :bug: fixed imageloading bug

--- dev/utils.py
+++ dev/utils.py
@@ -15,7 +15,24 @@ def zip2d(arg0, arg1, arg2):
     return arr
 
 # load image
-def loadImage(path, name, transpose=True, resize=(650, 450), invert=True, color=False, binary=False):
+def loadImage(path, name, transpose=False, resize=(650, 450), invert=False, color=False, binary=False):
+    image = cv2.imread('{path}/{name}'.format(path=path,name=name))
+    image = cv2.resize(image, resize, interpolation = cv2.INTER_AREA)
+    if not color:
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+        if binary:
+            for y in range(0, len(image)):
+                for x in range(0, len(image[y])):
+                    if image[y][x] < 125:
+                        image[y][x] = 0
+                    else:
+                        image[y][x] = 255
+    else:
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+    return np.asarray(image)
+
+def load1Pixel(path, name,color=False,binary=False):
     image = cv2.imread('{path}/{name}'.format(path=path,name=name))
     if not color:
         image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
@@ -26,12 +43,9 @@ def loadImage(path, name, transpose=True, resize=(650, 450), invert=True, color=
                         image[y][x] = 0
                     else:
                         image[y][x] = 255
+    else:
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-    image = cv2.resize(image, resize, interpolation = cv2.INTER_AREA)
-    if invert:
-        image = 255 - image
-    if transpose:
-        image = np.transpose(image)
     return np.asarray(image)
 
 def thinnImage(image):
@@ -60,3 +74,60 @@ def mapPixel(image,function):
         for x in range(0,len(image[y])):
             newImage[y][x] = function(image[y][x])
     return newImage
+
+# coords can either be a array with x y value or two seperate arguments with x and y
+def getPixel(image,*coords):
+    if len(coords) == 1:
+        try:
+            return image[coords[0][1]][coords[0][0]]
+        except:
+            return image[0][0]
+    else:
+        try:
+            return image[coords[1]][coords[0]]
+        except:
+            return image[0][0]
+        
+
+# coords can either be a array with x y value or two seperate arguments with x and y
+def setPixel(image,color,*coords):
+    if len(coords) == 1:
+        image[coords[0][1]][coords[0][0]] = color
+        return image
+    else:
+        image[coords[1]][coords[0]] = color
+        return image
+
+# colors all the given pixels in an image
+def colorPixels(image,pixels,color):
+    for visitedPixel in pixels:
+        image = setPixel(image,color,visitedPixel[0],visitedPixel[1])
+    return image
+
+# check if the whole image has only one Color
+def isOneColor(image,color):
+    for i in image:
+        for j in i:
+            if not j == color:
+                return False
+    return True
+
+# Draws a Rectangle
+def drawRect(image,boundingBoxes,color):
+    corner1 = boundingBoxes[0]
+    corner2 = boundingBoxes[1]
+    startX = corner1[0]
+    startY = corner1[1]
+
+    endX = corner2[0]
+    endY = corner2[1]
+
+    for x in range(startX,endX):
+        image[startY][x] = color
+        image[endY][x] = color
+
+    for y in range(startY,endY):
+        image[y][startX] = color
+        image[y][endX] = color
+
+    return image
