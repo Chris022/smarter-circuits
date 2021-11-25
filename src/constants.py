d77bfa8fd1c70e6e80a9d056298ac4b2bf939ba0
Makai04 <43985854+Makei04@users.noreply.github.com>
2021-11-25 10:33:22 +0100

    :construction: added component classification as library

---
+++ src/constants.py
@@ -0,0 +1,11 @@
+INTERSECTION_COLOR = "red"
+END_COLOR = "blue"
+CORNER_COLOR = "yellow"
+OTHER_NODE_COLOR = "white"
+
+OTHER_EDGE_COLOR = "green"
+
+FOREGROUND = 0
+BACKGROUND = 255
+
+CLASS_NAMES = ['capacitor', 'ground', 'resistor']
