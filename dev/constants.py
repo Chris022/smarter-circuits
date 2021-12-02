0e0f75066bb62cd6def09d40d2ecbb862be5a4d7
unknown <christoph02.cw@gmail.com>
2021-12-02 09:02:40 +0100

    :consturction: added new constants

---
+++ dev/constants.py
@@ -0,0 +1,15 @@
+INTERSECTION_COLOR = "red"
+END_COLOR = "blue"
+CORNER_COLOR = "yellow"
+OTHER_NODE_COLOR = "white"
+COMPONENT_NODE_COLOR = "green"
+
+OTHER_EDGE_COLOR = "green"
+
+FOREGROUND = 0
+BACKGROUND = 255
+
+CLASS_NAMES = ['capacitor', 'ground', 'inductor', 'resistor']
+
+
+RESISTOR_TYPE = "resistor"
