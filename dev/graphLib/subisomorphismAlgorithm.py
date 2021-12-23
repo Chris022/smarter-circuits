d5915e00d48bf0c500c23f2381eb149e645c1565
Makei04 <mayrhofer803@gmx.at>
2021-12-23 11:44:50 +0100

    :construction: testing patternMatching

--- dev/graphLib/subisomorphismAlgorithm.py
+++ dev/graphLib/subisomorphismAlgorithm.py
@@ -1,6 +1,83 @@
 from graphLib.graph import Graph,Vertex,Edge,Table
 import copy
 
+#create Graph
+#heystack = Graph()
+#
+#v0 = Vertex("blue")
+#v1 = Vertex("red")
+#v2 = Vertex("blue")
+#v3 = Vertex("red")
+#v4 = Vertex("red")
+#v5 = Vertex("red")
+#v6 = Vertex("blue")
+#v7 = Vertex("blue")
+#v8 = Vertex("red")
+#v9 = Vertex("yellow")
+#v10 = Vertex("yellow")
+#v11 = Vertex("red")
+#v12 = Vertex("yellow")
+#v13 = Vertex("yellow")
+#v14 = Vertex("red")
+#v15 = Vertex("red")
+#v16 = Vertex("blue")
+#v17 = Vertex("blue")
+#v18 = Vertex("red")
+#v19 = Vertex("blue")
+#v20 = Vertex("red")
+#v21 = Vertex("blue")
+#heystack.addVertices([v0,v1,v2,v3,v4,v5,v6,v7,v8,v9,v10,v11,v12,v13,v14,v15,v16,v17,v18,v19,v20,v21])
+#heystack.addEdge(Edge(),v0.id,v1.id)
+#heystack.addEdge(Edge(),v1.id,v2.id)
+#heystack.addEdge(Edge(),v1.id,v3.id)
+#heystack.addEdge(Edge(),v3.id,v4.id)
+#heystack.addEdge(Edge(),v4.id,v5.id)
+#heystack.addEdge(Edge(),v5.id,v6.id)
+#heystack.addEdge(Edge(),v5.id,v7.id)
+#heystack.addEdge(Edge(),v4.id,v8.id)
+#heystack.addEdge(Edge(),v8.id,v9.id)
+#heystack.addEdge(Edge(),v9.id,v10.id)
+#heystack.addEdge(Edge(),v10.id,v11.id)
+#heystack.addEdge(Edge(),v11.id,v12.id)
+#heystack.addEdge(Edge(),v12.id,v13.id)
+#heystack.addEdge(Edge(),v13.id,v8.id)
+#heystack.addEdge(Edge(),v11.id,v14.id)
+#heystack.addEdge(Edge(),v14.id,v15.id)
+#heystack.addEdge(Edge(),v15.id,v16.id)
+#heystack.addEdge(Edge(),v15.id,v17.id)
+#heystack.addEdge(Edge(),v14.id,v18.id)
+#heystack.addEdge(Edge(),v18.id,v13.id)
+#heystack.addEdge(Edge(),v18.id,v20.id)
+#heystack.addEdge(Edge(),v20.id,v19.id)
+#heystack.addEdge(Edge(),v20.id,v21.id)
+#
+#
+#needle = Graph()
+#v22 = Vertex("blue")
+#v23 = Vertex("red")
+#v24 = Vertex("blue")
+#needle.addVertex(v22)
+#needle.addVertex(v23)
+#needle.addVertex(v24)
+#needle.addEdge(Edge(),v22.id,v23.id)
+#needle.addEdge(Edge(),v23.id,v24.id)
+
+#needle = Graph()
+#v1 = Vertex(color="red")
+#v2 = Vertex(color="yellow")
+#v3 = Vertex(color="yellow")
+#v4 = Vertex(color="red")
+#v5 = Vertex(color="yellow")
+#v6 = Vertex(color="yellow")
+#needle.addVertices([v1,v2,v3,v4,v5,v6])
+#needle.addEdge(Edge(), v1.id, v2.id)
+#needle.addEdge(Edge(), v2.id, v3.id)
+#needle.addEdge(Edge(), v3.id, v4.id)
+#needle.addEdge(Edge(), v4.id, v5.id)
+#needle.addEdge(Edge(), v5.id, v6.id)
+#needle.addEdge(Edge(), v6.id, v1.id)
+
+
 def isValidIsomorphism(heystack,needle,matchingTable):
     needleVertexIds = matchingTable.rows
     for needlesVertexId in needleVertexIds:
@@ -51,10 +128,33 @@ def generateMatchingTable(heystack,needle):
                 )
     return matchingTable
 
+def simplify(heystack,needle,matchingTable):
 
-def depthFirstSearch(heystack,needle):
+    # for every 1 in the Table
+    for y in matchingTable.rows:
+        for x in matchingTable.columns:
+            if matchingTable.getValue(y,x) == 1:
+                
+                #get the neighbours of the needles
+                needleNeighbours = needle.getNeighborIds(y)
+
+                #get the neighbours of the heystack
+                heystackNeighbours = heystack.getNeighborIds(x)
+
+                #for every needle neighbour
+                match = False
+                for needleNeighbour in needleNeighbours:
+                    for heystackNeighbour in heystackNeighbours:
+                        if matchingTable.getValue(needleNeighbour,heystackNeighbour) == 1:
+                            match = True
+                if not match:
+                    matchingTable.setValue(y,x,0)
+        
+    return matchingTable
+
+
+def depthFirstSearch(heystack,needle,matchingTable):
     matches = []
-    matchingTable = generateMatchingTable(heystack,needle)
     def recursive(currentRow,usedColumns,matchingTable):
 
         #Create independant copies of arguments
@@ -74,9 +174,13 @@ def depthFirstSearch(heystack,needle):
                 matches.append(matchingTable)
             return
 
+        matchingTable = simplify(heystack,needle,matchingTable)
+
         #For every unused Column
         for columnName in matchingTable.columns:
             if not columnName in usedColumns:
+                if not matchingTable.getValue(matchingTable.rows[currentRow],columnName) == 1:
+                    continue
                 #set the column to be used
                 usedColumns.append(columnName)
                 #set the whole row to 0 accept the used column
@@ -86,8 +190,12 @@ def depthFirstSearch(heystack,needle):
                 recursive(currentRow+1,usedColumns,newTable)
                 #unset column
                 usedColumns.remove(columnName)
+        return
     recursive(0,[],matchingTable)
     return matches
+
+
+
 
 def convertMatchingTablesToList(heystack,matchingTables):
     matchingsList = []
@@ -102,6 +210,13 @@ def convertMatchingTablesToList(heystack,matchingTables):
     return matchingsList
 
 def subisomorphism(heystack,needle):
-    solutions = depthFirstSearch(heystack,needle)
+    print("1")
+    matchingTable = generateMatchingTable(heystack,needle)
+    print("2")
+    matchingTable = simplify(heystack,needle,matchingTable)
+    print(matchingTable)
+    solutions = depthFirstSearch(heystack,needle,matchingTable)
+    print("4")
     matches = convertMatchingTablesToList(heystack,solutions)
+    print("5")
     return matches
