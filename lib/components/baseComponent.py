962254fbac6bf73ab35d20790dd387da74d85fe5
unknown <christoph02.cw@gmail.com>
2022-01-12 16:00:31 +0100

    :construction: implement different rotations

--- lib/components/baseComponent.py
+++ lib/components/baseComponent.py
@@ -1,7 +1,35 @@
 from abc import ABC, abstractmethod
+from lib.constants import POSSIBLE_ROTATIONS
 
 class BaseComponent(ABC):
-    
+
+    @staticmethod
+    def getMeasurePoint(rotatonOffset, rotation,intersectionVertices):
+        xPositions = fmap (lambda x: x.attr["coordinates"][0],intersectionVertices)
+        yPositions = fmap (lambda x: x.attr["coordinates"][1],intersectionVertices)
+        rot = rotatonOffset + rotation
+        if      rot == POSSIBLE_ROTATIONS[0]:
+            x = min(xPositions)
+            y = min(yPositions)
+
+            x -= 100
+        elif    rot == POSSIBLE_ROTATIONS[1]:
+            x = min(xPositions)
+            y = max(yPositions)
+
+            y += 100
+        elif    rot == POSSIBLE_ROTATIONS[3]:
+            x = max(xPositions)
+            y = max(yPositions)
+
+            x += 100
+        elif    rot == POSSIBLE_ROTATIONS[3]:
+            x = max(xPositions)
+            y = min(yPositions)
+
+            y -= 100
+        return (x,y)
+
     @staticmethod
     @abstractmethod
     def connect(rotation,intersectionVertices):
--- lib/components/resistor.py
+++ lib/components/resistor.py
@@ -10,14 +10,8 @@ class Resistor(BaseComponent):
 
     @staticmethod
     def connect(rotation,intersectionVertices):
-        #get lowest Y and X coordinate
-        xPositions = fmap (lambda x: x.attr["coordinates"][0],intersectionVertices)
-        yPositions = fmap (lambda x: x.attr["coordinates"][1],intersectionVertices)
-        x = min(xPositions)
-        y = min(yPositions)
-
-        # make the x direction smaler by 100
-        x -= 100
+        
+        basePos = BaseComponent().getMeasurePoint(0,rotation,intersectionVertices)
     
         #now get the distance from the (x|y) point to each intersection
         #and map the smalles to connection 0, the second smallest to 1 ...
@@ -25,7 +19,7 @@ class Resistor(BaseComponent):
         for intersectionVertex in intersectionVertices:
             position = intersectionVertex.attr["coordinates"]
             
-            distance = dist((x,y),position)
+            distance = dist(basePos,position)
             distances.append((distance,position))
 
         #sort distances
