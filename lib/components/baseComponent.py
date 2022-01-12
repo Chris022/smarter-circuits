d51bbf864e84d1175fbefd9fa37bd0db367b0fef
unknown <christoph02.cw@gmail.com>
2022-01-12 16:10:32 +0100

    :bug: fix circular import

--- lib/components/baseComponent.py
+++ lib/components/baseComponent.py
@@ -1,5 +1,4 @@
 from abc import ABC, abstractmethod
-from lib.constants import POSSIBLE_ROTATIONS
 from lib.utils import fmap
 
 class BaseComponent(ABC):
@@ -9,22 +8,22 @@ class BaseComponent(ABC):
         xPositions = fmap (lambda x: x.attr["coordinates"][0],intersectionVertices)
         yPositions = fmap (lambda x: x.attr["coordinates"][1],intersectionVertices)
         rot = rotatonOffset + rotation
-        if      rot == POSSIBLE_ROTATIONS[0]:
+        if      rot == 0:
             x = min(xPositions)
             y = min(yPositions)
 
             x -= 100
-        elif    rot == POSSIBLE_ROTATIONS[1]:
+        elif    rot == 90:
             x = min(xPositions)
             y = max(yPositions)
 
             y += 100
-        elif    rot == POSSIBLE_ROTATIONS[3]:
+        elif    rot == 180:
             x = max(xPositions)
             y = max(yPositions)
 
             x += 100
-        elif    rot == POSSIBLE_ROTATIONS[3]:
+        elif    rot == 270:
             x = max(xPositions)
             y = min(yPositions)
 
