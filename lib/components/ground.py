19a838fca05b7ba15dc2def3c6a6551b56af3b88
unknown <christoph02.cw@gmail.com>
2022-01-12 15:44:13 +0100

    :construction: add Ground Component

---
+++ lib/components/ground.py
@@ -0,0 +1,15 @@
+from lib.utils import fmap
+from math import dist
+
+from lib.components.baseComponent import BaseComponent
+
+#             |
+# #0----------|
+#             |
+class Ground(BaseComponent):
+
+    @staticmethod
+    def connect(rotation,intersectionVertices):
+        mapings = {0: intersectionVertices[0].attr["coordinates"] }
+
+        return mapings
--- lib/constants.py
+++ lib/constants.py
@@ -1,4 +1,5 @@
 from lib.components.resistor import Resistor
+from lib.components.ground import Ground
 
 INTERSECTION_COLOR = "red"
 END_COLOR = "blue"
@@ -13,6 +14,6 @@ FOREGROUND = 0
 BACKGROUND = 255
 
 CLASS_NAMES = {"cap":'capacitor', "gnd":'ground', "ind":'inductor', "res":'resistor'}
-CLASS_OBJECTS = {"resistor":Resistor(),}
+CLASS_OBJECTS = {"resistor":Resistor(),"ground":Ground()}
 
 INTERSECTION_COMBINATION_DIST = 3
