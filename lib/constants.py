d6bc7488f1b33fd77c77b30339004ecca2d5ae89
Makai04 <43985854+Makei04@users.noreply.github.com>
2022-01-16 21:34:57 +0100

    :sparkles: yolo component detection

--- lib/constants.py
+++ lib/constants.py
@@ -8,6 +8,17 @@ OTHER_EDGE_COLOR = "green"
 FOREGROUND = 0
 BACKGROUND = 255
 
-CLASS_NAMES = ['capacitor', 'ground', 'inductor', 'resistor']
-
 INTERSECTION_COMBINATION_DIST = 3
+
+CLASS_NAMES = ['capacitor', 'ground', 'inductor', 'resistor', 'voltage']
+
+CLASS_COLORS = [(0,0,255), (255,0,255), (47,145,28), (255,0,0), (196,196,33)]
+
+
+S = 7
+B = 1 #only works with B=1
+C = len(CLASS_NAMES)
+INPUT_SIZE = 448
+
+LAMBDA_COORD = 5
+LAMBDA_NOOBJ = 0.5
